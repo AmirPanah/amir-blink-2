9dd12ce3662e62c7fff64f1cf34ea59ad83b1ebb
AmirPanah <amir.panah2020@gmail.com>
2026-02-02 13:02:47 +0100

    all LED blink

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,28 @@ fn main() -> ! {
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    let mut led = gpioe.pe9.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
+    // LEDها به ترتیب
+    let mut led4 = gpioe.pe8.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led3 = gpioe.pe9.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led5 = gpioe.pe10.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led7 = gpioe.pe11.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led9 = gpioe.pe12.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led10 = gpioe.pe13.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led8 = gpioe.pe14.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut led6 = gpioe.pe15.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    let delay = clocks.sysclk().0 / 6;
 
     loop {
-        led.toggle().ok();
-        cortex_m::asm::delay(clocks.sysclk().0 / 2);
+        led4.set_high().ok();  cortex_m::asm::delay(delay); led4.set_low().ok();
+        led3.set_high().ok();  cortex_m::asm::delay(delay); led3.set_low().ok();
+        led5.set_high().ok();  cortex_m::asm::delay(delay); led5.set_low().ok();
+        led7.set_high().ok();  cortex_m::asm::delay(delay); led7.set_low().ok();
+        led9.set_high().ok();  cortex_m::asm::delay(delay); led9.set_low().ok();
+        led10.set_high().ok(); cortex_m::asm::delay(delay); led10.set_low().ok();
+        led8.set_high().ok();  cortex_m::asm::delay(delay); led8.set_low().ok();
+        led6.set_high().ok();  cortex_m::asm::delay(delay); led6.set_low().ok();
     }
 }
+
